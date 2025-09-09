59c416d8f1a80dd2f3057a8b61e895de4476a031
GnarMaster <zzgo0402.nexon.com@gmail.com>
2025-09-09 17:20:22 +0900

    Create util.py

---
+++ util.py
@@ -0,0 +1,28 @@
+import os
+import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def safe_int(val):
+    try:
+        return int(str(val).strip())
+    except:
+        return 0
+
+def get_job_icon(job: str) -> str:
+    icons = {
+        "백수": "🎖️",
+        "전사": "⚔️",
+        "마법사": "🔮",
+        "도적": "🥷",
+        "특수": "🎭",
+        "궁수": "🏹"
+    }
+    return icons.get(job, "🎖️")
+
+def get_sheet():
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds_dict = json.loads(os.getenv("GOOGLE_CREDS"))
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    client = gspread.authorize(creds)
+    return client.open("Discord_Message_Log").sheet1
