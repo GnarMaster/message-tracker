5f43083370c6915225e8911f79927357d1f84431
GnarMaster <zzgo0402.nexon.com@gmail.com>
2025-09-22 19:29:49 +0900

    Update voice_exp.py

--- cogs/voice_exp.py
+++ cogs/voice_exp.py
@@ -2,7 +2,7 @@
 # from discord.ext import commands
 # from datetime import datetime
 # from utils import get_sheet, safe_int
-
+ 
 # class VoiceExp(commands.Cog):
 #     def __init__(self, bot):
 #         self.bot = bot
